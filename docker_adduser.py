264cec4bb4ddf183821d679b419b9c1e0e948f2a
Tony Tromp <tony.tromp@gmail.com>
2024-03-16 21:44:44 +0100

    Dockerized update

--- docker_adduser.py
+++ docker_adduser.py
@@ -1,14 +1,16 @@
-import os
+import os, socket, time
 from datetime import datetime
 from uuid import uuid4
 from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime
 from sqlalchemy.orm import declarative_base, sessionmaker, scoped_session  # Updated import here
 from werkzeug.security import generate_password_hash
+from urllib.parse import urlparse
 
 # Editable database URI
-DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://sharewarez:!Piratingin2024!@localhost/sharewarez')
-
-
+#DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://sharewarez:!Piratingin2024!@localhost/sharewarez')
+from config import Config
+config = Config()
+DATABASE_URI = config.SQLALCHEMY_DATABASE_URI
 
 Base = declarative_base()
 
@@ -30,7 +32,33 @@ class User(Base):
     password_reset_token = Column(String(256), nullable=True)
     token_creation_time = Column(DateTime, nullable=True)
 
+
+
+def check_postgres_port_open(host, port, retries=5, delay=2):
+    """
+    Checks if the PostgreSQL port is open by attempting to create a socket connection.
+    If the connection attempt fails, it waits for 'delay' seconds and retries.
+    
+    :param host: The hostname or IP address of the PostgreSQL server.
+    :param port: The port number of the PostgreSQL server.
+    :param retries: Maximum number of retries.
+    :param delay: Delay in seconds between retries.
+    :return: True if the port is open, False otherwise.
+    """
+    for attempt in range(retries):
+        try:
+            with socket.create_connection((host, port), timeout=10):
+                print(f"Connection to PostgreSQL on port {port} successful.")
+                return True
+        except (socket.timeout, ConnectionRefusedError):
+            print(f"Connection to PostgreSQL on port {port} failed. Attempt {attempt + 1} of {retries}.")
+            time.sleep(delay)
+    return False
+
 def create_admin_user():
+    parsed_url = urlparse(DATABASE_URI)
+    check_postgres_port_open(parsed_url.hostname, 5432, 60, 2);
+
     engine = create_engine(DATABASE_URI)
     Base.metadata.bind = engine
     DBSession = sessionmaker(bind=engine)
