f9bf827bf59e072eafa21e2efb3ebe630a0062ea
Acehighness <wateraxe@gmail.com>
2024-03-18 18:12:23 +0100

    launching 0.1

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,6 @@ FROM python:3.9-slim-buster
 
 WORKDIR /app
 
-RUN apt-get update -y
-RUN apt-get install netcat -y
-
 COPY . .
 
 RUN pip install -r requirements.txt
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-ls -l /app
+# List the contents of the /app directory
+echo "SharewareZ container strapping boots"
 
+# Function to check if PostgreSQL is ready
+wait_for_postgres() {
+    until psql -h <database_host> -U <database_user> -c '\q' &>/dev/null; do
+        echo "Waiting for PostgreSQL to become available..."
+        sleep 5
+    done
+    echo "PostgreSQL is now available."
+}
+
+# Wait for PostgreSQL to come online
+wait_for_postgres
+
+# Execute your Python scripts
 python /app/docker_adduser.py
 python /app/app.py
