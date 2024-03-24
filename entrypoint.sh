bad2b0ccfb757d3a96334e747fec608634763a10
Acehighness <wateraxe@gmail.com>
2024-03-24 15:34:35 +0100

    docker patch

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,17 @@
 
 # List the contents of the /app directory
 echo "SharewareZ container strapping boots"
+# Function to check if PostgreSQL is ready
+wait_for_postgres() {
+    until psql -h <database_host> -U <database_user> -c '\q' &>/dev/null; do
+        echo "Waiting for PostgreSQL to become available..."
+        sleep 5
+    done
+    echo "PostgreSQL is now available."
+}
 
+# Wait for PostgreSQL to come online
+wait_for_postgres
 
 # Execute your Python scripts
 python /app/docker_adduser.py
