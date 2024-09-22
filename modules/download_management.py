4ed135a01144570e889f5a11cc7811cd0d6322c7
Acehighness <wateraxe@gmail.com>
2024-09-22 12:15:58 +0200

    cleanup

--- modules/download_management.py
+++
@@ -1,6 +0,0 @@
-# File: /modules/download_management.py
-# This file contains download management utilities extracted from routes.py
-
-def handle_download_request(download_id):
-    # Placeholder for handling a download request
-    pass
