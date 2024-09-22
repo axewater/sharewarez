6d80f410f923e6504189b59e6d13565435f936a0
Acehighness <wateraxe@gmail.com>
2024-09-22 09:06:42 +0200

    cleanup prep for theming admin html files have no more inline styling admin html files have no more inline javascript fixed username check js (csrf token was missing)

--- modules/static/js/checkusername.js
+++ modules/static/js/checkusername.js
@@ -12,30 +12,41 @@ document.getElementById('username').addEventListener('input', function() {
 
   // Wait for 500ms of inactivity before sending the request
   timeout = setTimeout(function() {
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
     fetch('/api/check_username', {
       method: 'POST',
       body: JSON.stringify({
         username: username
       }),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrfToken
+      },
+      credentials: 'same-origin'
     })
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
     .then(data => {
-      let usernameCheckResultDiv = document.getElementById('username-check-result');
+      let usernameCheckResultDiv = document.getElementById('usernameAvailabilityFeedback');
       if (data.exists) {
         usernameCheckResultDiv.textContent = "Username not available";
         usernameCheckResultDiv.style.color = "red";
-        document.getElementById('user-detail-form').querySelector('input[type="submit"]').disabled = true;
+        document.getElementById('createUserForm').querySelector('button[type="submit"]').disabled = true;
       } else {
         usernameCheckResultDiv.textContent = "Username available";
         usernameCheckResultDiv.style.color = "green";
-        document.getElementById('user-detail-form').querySelector('input[type="submit"]').disabled = false;
+        document.getElementById('createUserForm').querySelector('button[type="submit"]').disabled = false;
       }
     })
     .catch((error) => {
       console.error('Error:', error);
+      let usernameCheckResultDiv = document.getElementById('usernameAvailabilityFeedback');
+      usernameCheckResultDiv.textContent = "Error checking username availability";
+      usernameCheckResultDiv.style.color = "orange";
     });
   }, 500);
 });
