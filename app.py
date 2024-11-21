e192ab1a971137f3920e8aafb605e02b43514756
Bovive <4776549+Bovive@users.noreply.github.com>
2024-11-20 22:01:53 -0600

    Add Ability to Filter Extensions
    Add ability for the user to filter file extensions from the dynamic scanning. It will tell Watchdog to ignore any files with those extensions.
    
    Also fixed missing argument in get_game_by_full_disk_path function.

--- app.py
+++ app.py
@@ -24,16 +24,21 @@ shutdown_event = threading.Event()
 class MyHandler(FileSystemEventHandler):  
     global last_trigger_time
     last_trigger_time = time.time()
-    
+
     def on_created(self, event):
         if event.src_path.find('~') == -1:
             with app.app_context():
-                from modules.utilities import discord_update
-                from modules.models import GlobalSettings
-                settings = GlobalSettings.query.first()
-                if settings.enable_game_updates or settings.enable_game_extras:
-                    print(f"Event: {event.src_path} was {event.event_type}")
-                    discord_update(event.src_path, event.event_type)            
+                ignore_ext = current_app.config['MONITOR_IGNORE_EXT']  # List of extensions to ignore.
+                file_name = event.src_path.split('/')[-1]
+                file_ext = file_name.split('.')[-1]
+                print(f"File Name: {file_name} and File Extension {file_ext}")
+                if file_ext not in ignore_ext:
+                    from modules.utilities import discord_update
+                    from modules.models import GlobalSettings
+                    settings = GlobalSettings.query.first()
+                    if settings.enable_game_updates or settings.enable_game_extras:
+                        print(f"Event: {event.src_path} was {event.event_type}")
+                        discord_update(event.src_path, event.event_type)            
     def on_modified(self, event):
         global last_trigger_time
         global last_modified
@@ -43,12 +48,17 @@ class MyHandler(FileSystemEventHandler):
                 last_modified = event.src_path
                 last_trigger_time = current_time
                 with app.app_context():
-                    from modules.utilities import discord_update
-                    from modules.models import GlobalSettings
-                    settings = GlobalSettings.query.first()
-                    if settings.enable_main_game_updates:
-                        print(f"Event: {event.src_path} was {event.event_type}")
-                        discord_update(event.src_path, event.event_type) 
+                    ignore_ext = current_app.config['MONITOR_IGNORE_EXT']  # List of extensions to ignore.
+                    file_name = event.src_path.split('/')[-1]
+                    file_ext = file_name.split('.')[-1]
+                    print(f"File Name: {file_name} and File Extension {file_ext}")
+                    if file_ext not in ignore_ext:
+                        from modules.utilities import discord_update
+                        from modules.models import GlobalSettings
+                        settings = GlobalSettings.query.first()
+                        if settings.enable_main_game_updates:
+                            print(f"Event: {event.src_path} was {event.event_type}")
+                            discord_update(event.src_path, event.event_type) 
         
 def watch_directory(path):
     observer = Observer()
