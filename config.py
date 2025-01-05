e4f497f0f1c030c78ed998689fc2065b4a7bade3
Acehighness <wateraxe@gmail.com>
2025-01-05 21:11:10 +0100

    feat: add configuration settings for database and file paths

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+class Config(object):
+    # Set Database connection string here or in your .env file
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/sharewarez')
+    
+    # Set the path to the folder where the game files are stored (ie: use c:\gamez for windows or /gamez for linux)
+    DATA_FOLDER_WAREZ = os.getenv('DATA_FOLDER_WAREZ', '/')
+    
+    # OS-specific base folder paths
+    if os.name == 'nt':  # Windows
+        BASE_FOLDER_WINDOWS = os.getenv('BASE_FOLDER_WINDOWS', 'Z:\\')
+    else:  # POSIX (Linux, Unix, MacOS, etc.)
+        BASE_FOLDER_POSIX = os.getenv('BASE_FOLDER_POSIX', '/')
+
+    # YOU CAN LEAVE ALL THESE SETTINGS AT DEFAULT:
+    UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'modules/static/library')
+    IMAGE_SAVE_PATH = os.path.join(os.path.dirname(__file__), 'modules/static/library/images')
+    ZIP_SAVE_PATH = os.path.join(os.path.dirname(__file__), 'modules/static/library/zips')
+    IGDB_API_ENDPOINT = os.getenv('IGDB_API_ENDPOINT', 'https://api.igdb.com/v4/games')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY') or 'you-will-never-guess-this-dont-hate-docker-users'
