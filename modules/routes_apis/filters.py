6ddef5333032f82fc0ee68b4b6a10e43db24e923
axewater <wateraxe@gmail.com>
2025-07-20 19:19:00 +0200

    big refactor / modularized routes

--- modules/routes_apis/filters.py
+++ modules/routes_apis/filters.py
@@ -1,43 +1,33 @@
-from flask import jsonify, request, current_app
+# /modules/routes_apis/filters.py
+from flask import jsonify
 from flask_login import login_required
 from modules.models import Genre, Theme, GameMode, PlayerPerspective
-from modules.utils_processors import get_global_settings
-from flask import Blueprint
-from modules import cache
+from . import apis_bp
 
-apis_filters_bp = Blueprint('apis_filters', __name__)
-
-@apis_filters_bp.context_processor
-@cache.cached(timeout=500, key_prefix='global_settings')
-def inject_settings():
-    """Context processor to inject global settings into templates"""
-    return get_global_settings()
-
-@apis_filters_bp.route('/api/genres')
+@apis_bp.route('/genres')
 @login_required
 def get_genres():
     genres = Genre.query.all()
     genres_list = [{'id': genre.id, 'name': genre.name} for genre in genres]
     return jsonify(genres_list)
 
-@apis_filters_bp.route('/api/themes')
+@apis_bp.route('/themes')
 @login_required
 def get_themes():
     themes = Theme.query.all()
     themes_list = [{'id': theme.id, 'name': theme.name} for theme in themes]
     return jsonify(themes_list)
 
-@apis_filters_bp.route('/api/game_modes')
+@apis_bp.route('/game_modes')
 @login_required
 def get_game_modes():
     game_modes = GameMode.query.all()
     game_modes_list = [{'id': game_mode.id, 'name': game_mode.name} for game_mode in game_modes]
     return jsonify(game_modes_list)
 
-@apis_filters_bp.route('/api/player_perspectives')
+@apis_bp.route('/player_perspectives')
 @login_required
 def get_player_perspectives():
     perspectives = PlayerPerspective.query.all()
     perspectives_list = [{'id': perspective.id, 'name': perspective.name} for perspective in perspectives]
     return jsonify(perspectives_list)
-
