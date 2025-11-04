1dc6481cc7838aeb3c8cb12bd1119fde0c2f966d
axewater <wateraxe@gmail.com>
2025-11-04 18:10:23 +0100

    time to fix test coverage

--- modules/routes_games_ext/__init__.py
+++ modules/routes_games_ext/__init__.py
@@ -5,7 +5,7 @@ from modules.utils_processors import get_global_settings
 games_bp = Blueprint('games', __name__)
 
 @games_bp.context_processor
-@cache.cached(timeout=10, key_prefix='global_settings')
+@cache.cached(timeout=20, key_prefix='global_settings')
 def inject_settings():
     """Context processor to inject global settings into templates"""
     return get_global_settings()
